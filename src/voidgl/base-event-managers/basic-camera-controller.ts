9b6fa175972195f13fc37dfbc9a0ffe52178296c
T <t@Ultron>
2018-04-26 03:29:18 -0400

    WIP

--- src/voidgl/base-event-managers/basic-camera-controller.ts
+++ src/voidgl/base-event-managers/basic-camera-controller.ts
@@ -9,6 +9,15 @@ export interface IBasicCameraControllerOptions {
   /** When this is set to true, the start view can be targetted even when behind other views */
   ignoreCoverViews?: boolean;
   /**
+   * This is a filter applied to the offset that comes from the chart camera.
+   */
+  filterPan?(panDelta: [number, number, number], view: View) : [number, number, number];
+  /**
+   * This is a filter applied to the scale that comes from the chart camera.
+   */
+  filterScale?(scaleDelta: [number, number, number], view: View): [number, number, number];
+
+  /**
    * This adjusts how fast scaling is applied from the mouse wheel
    */
   scaleFactor?: number;
@@ -22,6 +31,9 @@ export interface IBasicCameraControllerOptions {
 export class BasicCameraController extends EventManager {
   /** This is the camera that this controller will manipulate */
   camera: ChartCamera;
+//  P private filterPan = (panDelta: [number, number, number]) => panDelta;
+//  P private filterScale = (scaleDelta: [number, number, number]) => scaleDelta;
+
   /** When this is set to true, the start view can be targetted even when behind other views */
   ignoreCoverViews?: boolean;
   /** The rate scale is adjusted with the mouse wheel */
@@ -46,7 +58,21 @@ export class BasicCameraController extends EventManager {
       this.startViews = Array.isArray(options.startView) ? options.startView : [options.startView];
     }
   }
-
+  filterPan(panDelta: [number, number, number], view: View) : [
+    number, number, number] {
+    return [
+      view.panFilter[0] ? panDelta[0] : 0,
+      view.panFilter[1] ? panDelta[1] : 0,
+      view.panFilter[2] ? panDelta[2] : 0,
+    ];
+  }
+  filterScale(scaleDelta: [number, number, number], view: View) : [number, number, number] {
+    return ([
+      view.scaleFilter[0] ? scaleDelta[0] : 1,
+      view.scaleFilter[1] ? scaleDelta[1] : 1,
+      view.scaleFilter[2] ? scaleDelta[2] : 1,
+    ]);
+  }
   findCoveredStartView(e: IMouseInteraction) {
     const found = e.viewsUnderMouse.find(under => this.startViews.indexOf(under.view.id) > -1);
     this.startViewDidStart = Boolean(found);
@@ -80,8 +106,11 @@ export class BasicCameraController extends EventManager {
 
   handleDrag(e: IMouseInteraction, drag: IDragMetrics) {
     if (this.canStart(e.start.view.id)) {
-      this.camera.offset[0] += drag.screen.delta.x / this.camera.scale[0] * e.start.view.panDelta[0];
-      this.camera.offset[1] += drag.screen.delta.y / this.camera.scale[1] * e.start.view.panDelta[1];
+      this.camera.offset[0] += (drag.screen.delta.x / this.camera.scale[0]);
+      this.camera.offset[1] += (drag.screen.delta.y / this.camera.scale[1]);
+    }
+    if (this.filterPan) {
+      this.camera.offset = this.filterPan(this.camera.offset, e.start.view);
     }
   }
 
@@ -94,15 +123,20 @@ export class BasicCameraController extends EventManager {
       const beforeZoom = targetView.screenToWorld(e.screen.mouse);
 
       const currentZoomX = this.camera.scale[0] || 1.0;
-      this.camera.scale[0] = currentZoomX + (wheelMetrics.wheel[1] * targetView.scaleDelta[0]) / this.scaleFactor * currentZoomX;
+      this.camera.scale[0] = currentZoomX + wheelMetrics.wheel[1] / this.scaleFactor * currentZoomX;
+//    P  this.camera.scale[0] = currentZoomX + wheelMetrics.wheel[1] * filterScale[0] / this.scaleFactor * currentZoomX;
 
       const currentZoomY = this.camera.scale[1] || 1.0;
-      this.camera.scale[1] = currentZoomY + (wheelMetrics.wheel[1] * targetView.scaleDelta[1]) / this.scaleFactor * currentZoomY;
+      this.camera.scale[1] = currentZoomY + wheelMetrics.wheel[1] / this.scaleFactor * currentZoomY;
 
       const afterZoom = targetView.screenToWorld(e.screen.mouse);
 
       this.camera.offset[0] -= (beforeZoom.x - afterZoom.x) / targetView.pixelRatio;
       this.camera.offset[1] -= (beforeZoom.y - afterZoom.y) / targetView.pixelRatio;
+
+      if (this.filterScale) {
+        this.camera.scale = this.filterScale(this.camera.scale, targetView);
+      }
     }
   }
 

d1a5239684cd7f986278f8ec8f0cb1e0b666eeb5
Diniden <lasermatter@yahoo.com>
2018-04-26 09:25:44 -0500

    Fixed up an NPE with atlas resources

--- src/voidgl/surface/texture/sub-texture.ts
+++ src/voidgl/surface/texture/sub-texture.ts
@@ -8,25 +8,25 @@ export class SubTexture {
   /** Stores the aspect ratio of the image for quick reference */
   aspectRatio: number = 1.0;
   /** The id of the atlas this texture is located on */
-  atlasReferenceID: string;
+  atlasReferenceID: string = '';
   /** This is the actual texture of the atlas this resource is located on */
-  atlasTexture: Three.Texture;
+  atlasTexture: Three.Texture | null = null;
   /** This is the top left UV coordinate of the sub texture on the atlas */
-  atlasTL: IPoint;
+  atlasTL: IPoint = {x: 0, y: 0};
   /** This is the top right UV coordinate of the sub texture on the atlas */
-  atlasTR: IPoint;
+  atlasTR: IPoint = {x: 0, y: 0};
   /** This is the bottom left UV coordinate of the sub texture on the atlas */
-  atlasBL: IPoint;
+  atlasBL: IPoint = {x: 0, y: 0};
   /** This is the bottom right UV coordinate of the sub texture on the atlas */
-  atlasBR: IPoint;
+  atlasBR: IPoint = {x: 0, y: 0};
   /** This is the normalized width of the sub texture on the atlas */
-  widthOnAtlas: number;
+  widthOnAtlas: number = 0;
   /** This is the normalized height of the sub texture on the atlas */
-  heightOnAtlas: number;
+  heightOnAtlas: number = 0;
   /** This flag is set to false when the underlying texture is no longer valid */
-  isValid: boolean;
+  isValid: boolean = false;
   /** Width in pixels of the image on the atlas */
-  pixelWidth: number;
+  pixelWidth: number = 0;
   /** Height in pixels of the image on the atlas */
-  pixelHeight: number;
+  pixelHeight: number = 0;
 }

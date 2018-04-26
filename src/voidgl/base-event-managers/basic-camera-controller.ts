463aad9feed2f83ab63ff0d4e9042e89aa42ea7a
T <t@Ultron>
2018-04-26 12:36:11 -0400

    WIP

--- src/voidgl/base-event-managers/basic-camera-controller.ts
+++ src/voidgl/base-event-managers/basic-camera-controller.ts
@@ -9,15 +9,6 @@ export interface IBasicCameraControllerOptions {
   /** When this is set to true, the start view can be targetted even when behind other views */
   ignoreCoverViews?: boolean;
   /**
-   * This is a filter applied to the offset that comes from the chart camera.
-   */
-  filterPan?(panDelta: [number, number, number], view: View) : [number, number, number];
-  /**
-   * This is a filter applied to the scale that comes from the chart camera.
-   */
-  filterScale?(scaleDelta: [number, number, number], view: View): [number, number, number];
-
-  /**
    * This adjusts how fast scaling is applied from the mouse wheel
    */
   scaleFactor?: number;
@@ -26,20 +17,21 @@ export interface IBasicCameraControllerOptions {
    * If not provided, then dragging anywhere will adjust the camera
    */
   startView?: string | string[];
+  scaleFilter?: [number, number, number][];
+  panFilter?: [number, number, number][];
 }
 
 export class BasicCameraController extends EventManager {
   /** This is the camera that this controller will manipulate */
   camera: ChartCamera;
-//  P private filterPan = (panDelta: [number, number, number]) => panDelta;
-//  P private filterScale = (scaleDelta: [number, number, number]) => scaleDelta;
-
   /** When this is set to true, the start view can be targetted even when behind other views */
   ignoreCoverViews?: boolean;
   /** The rate scale is adjusted with the mouse wheel */
   scaleFactor: number;
   /** The view that must be the start or focus of the interactions in order for the interactions to occur */
   startViews: string[] | undefined;
+  panFilters: [number, number, number][] | undefined;
+  scaleFilters: [number, number, number][] | undefined;
 
   /**
    * This flag is set to true when a start view is targetted on mouse down even if it is not
@@ -57,22 +49,53 @@ export class BasicCameraController extends EventManager {
     if (options.startView) {
       this.startViews = Array.isArray(options.startView) ? options.startView : [options.startView];
     }
+
+    if (options.panFilter) {
+      this.panFilters = Array.isArray(options.panFilter) ? options.panFilter : [options.panFilter];
+    }
+
+    if (options.scaleFilter) {
+      this.scaleFilters = Array.isArray(options.scaleFilter) ? options.scaleFilter : [options.scaleFilter];
+    }
   }
-  filterPan(panDelta: [number, number, number], view: View) : [
-    number, number, number] {
-    return [
-      view.panFilter[0] ? panDelta[0] : 0,
-      view.panFilter[1] ? panDelta[1] : 0,
-      view.panFilter[2] ? panDelta[2] : 0,
-    ];
+
+  filterPan(startView: string, panDelta: [number, number, number]) : [number, number, number] {
+    let index: number;
+    if (this.startViews) {
+      index = this.startViews.indexOf(startView);
+    }
+
+    if (index !== -1 && this.panFilters[index]) {
+      return [
+        this.panFilters[index][0] === 1.0 ? panDelta[0] : this.panFilters[index][0] || 0,
+        this.panFilters[index][1] === 1.0 ? panDelta[1] : this.panFilters[index][1] || 0,
+        this.panFilters[index][2] === 1.0 ? panDelta[2] : this.panFilters[index][2] || 0,
+      ];
+    }
+    else {
+      return panDelta;
+    }
   }
-  filterScale(scaleDelta: [number, number, number], view: View) : [number, number, number] {
-    return ([
-      view.scaleFilter[0] ? scaleDelta[0] : 1,
-      view.scaleFilter[1] ? scaleDelta[1] : 1,
-      view.scaleFilter[2] ? scaleDelta[2] : 1,
-    ]);
+
+  filterScale(startView: string, scaleDelta: [number, number, number]) : [number, number, number] {
+    let index: number;
+
+    if (this.startViews) {
+      index = this.startViews.indexOf(startView);
+    }
+
+    if (index !== -1 && this.scaleFilters[index]) {
+      return [
+        this.scaleFilters[index][0] === 1.0 ? scaleDelta[0] : this.scaleFilters[index][0] || 0,
+        this.scaleFilters[index][1] === 1.0 ? scaleDelta[1] : this.scaleFilters[index][1] || 0,
+        this.scaleFilters[index][2] === 1.0 ? scaleDelta[2] : this.scaleFilters[index][2] || 0,
+      ];
+    }
+    else {
+      return scaleDelta;
+    }
   }
+
   findCoveredStartView(e: IMouseInteraction) {
     const found = e.viewsUnderMouse.find(under => this.startViews.indexOf(under.view.id) > -1);
     this.startViewDidStart = Boolean(found);
@@ -110,7 +133,7 @@ export class BasicCameraController extends EventManager {
       this.camera.offset[1] += (drag.screen.delta.y / this.camera.scale[1]);
     }
     if (this.filterPan) {
-      this.camera.offset = this.filterPan(this.camera.offset, e.start.view);
+      this.camera.offset = this.filterPan(e.start.view.id, this.camera.offset);
     }
   }
 
@@ -135,7 +158,7 @@ export class BasicCameraController extends EventManager {
       this.camera.offset[1] -= (beforeZoom.y - afterZoom.y) / targetView.pixelRatio;
 
       if (this.filterScale) {
-        this.camera.scale = this.filterScale(this.camera.scale, targetView);
+        this.camera.scale = this.filterScale(targetView.id, this.camera.scale);
       }
     }
   }

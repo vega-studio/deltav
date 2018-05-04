2d77c31303f66f2a69ec51cef660966140a2acd9
Diniden <lasermatter@yahoo.com>
2018-05-04 12:04:56 -0500

    Enhanced setting range controls for basic cam controller

--- src/voidgl/surface/event-manager.ts
+++ src/voidgl/surface/event-manager.ts
@@ -1,6 +1,7 @@
 import { Bounds } from '../primitives';
 import { IProjection } from '../types';
 import { IDragMetrics, IMouseInteraction, IWheelMetrics, MouseEventManager } from './mouse-event-manager';
+import { View } from './view';
 
 /**
  * Classes can extend this and override the methods to respond to events.
@@ -17,10 +18,23 @@ export abstract class EventManager {
   abstract handleDrag(e: IMouseInteraction, drag: IDragMetrics): void;
   abstract handleWheel(e: IMouseInteraction, wheel: IWheelMetrics): void;
 
+  /**
+   * This retrieves the projections for the view specified by the provided viewId.
+   */
   getProjection(viewId: string): IProjection | null {
     return this.mouseManager.getView(viewId);
   }
 
+  /**
+   * This retrieves the actual view for the view specified by the provided viewId.
+   */
+  getView(viewId: string): View | null {
+    return this.mouseManager.getView(viewId) || null;
+  }
+
+  /**
+   * This retrieves the screen bounds for the view specified by the provided viewId.
+   */
   getViewScreenBounds(viewId: string): Bounds | null {
     const view = this.mouseManager.getView(viewId);
 
@@ -31,6 +45,10 @@ export abstract class EventManager {
     return null;
   }
 
+  /**
+   * This is used internally which provides the parent MouseEventManager via the param mouseManager for this
+   * EventManager.
+   */
   setMouseManager(mouseManager: MouseEventManager) {
     this.mouseManager = mouseManager;
   }

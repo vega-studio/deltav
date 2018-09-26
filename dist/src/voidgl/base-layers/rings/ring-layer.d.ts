b88989579cf942e1dabbb449d6fb98cf7503411f
Autorelease Script <samuel+voidbot@voidray.co>
2018-09-26 18:30:26 +0000

    Release 1.14.0

--- dist/src/voidgl/base-layers/rings/ring-layer.d.ts
+++ dist/src/voidgl/base-layers/rings/ring-layer.d.ts
@@ -1,9 +1,15 @@
 import { Bounds, IPoint } from "../../primitives";
 import { ILayerProps, IModelType, Layer } from "../../surface/layer";
 import { IMaterialOptions, IProjection, IShaderInitialization } from "../../types";
+import { IAutoEasingMethod, Vec } from "../../util";
 import { RingInstance } from "./ring-instance";
 export interface IRingLayerProps<T extends RingInstance> extends ILayerProps<T> {
     scaleFactor?(): number;
+    animate?: {
+        color?: IAutoEasingMethod<Vec>;
+        center?: IAutoEasingMethod<Vec>;
+        radius?: IAutoEasingMethod<Vec>;
+    };
 }
 export declare class RingLayer<T extends RingInstance, U extends IRingLayerProps<T>> extends Layer<T, U> {
     getInstancePickingMethods(): {

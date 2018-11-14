7060684989b39afff0e56b2d2cdbdfc920d3acf3
Autorelease Script <samuel+voidbot@voidray.co>
2018-11-14 03:44:20 +0000

    Release 1.20.3

--- dist/test/examples/vertex-attribute-packing.d.ts
+++ dist/test/examples/vertex-attribute-packing.d.ts
@@ -5,8 +5,8 @@ export declare class VertexAttributePacking extends BaseExample {
     rectangleProvider: InstanceProvider<RectangleInstance>;
     doesVertexpack: boolean;
     constructor(noVertexPacking?: boolean);
-    makeLayer(scene: string, _atlas: string, provider: InstanceProvider<EdgeInstance>): [import("src/voidgl/surface/layer-surface").ILayerConstructable<import("src/voidgl/instance-provider/instance").Instance> & {
-        defaultProps: import("src/voidgl/surface/layer").ILayerProps<import("src/voidgl/instance-provider/instance").Instance>;
-    }, import("src/voidgl/surface/layer").ILayerProps<import("src/voidgl/instance-provider/instance").Instance>][];
+    makeLayer(scene: string, _atlas: string, provider: InstanceProvider<EdgeInstance>): [import("../../src/voidgl/surface/layer-surface").ILayerConstructable<import("../../src/voidgl/instance-provider/instance").Instance> & {
+        defaultProps: import("../../src/voidgl/surface/layer").ILayerProps<import("../../src/voidgl/instance-provider/instance").Instance>;
+    }, import("../../src/voidgl/surface/layer").ILayerProps<import("../../src/voidgl/instance-provider/instance").Instance>][];
     makeProvider(): InstanceProvider<EdgeInstance>;
 }

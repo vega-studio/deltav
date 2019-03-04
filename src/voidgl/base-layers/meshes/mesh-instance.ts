a158cf27db06614a558584740950c5bda6233f88
heryoyo1989 <heryoyo1989@gmail.com>
2019-03-04 10:30:29 -0600

    added obj folder

--- src/voidgl/base-layers/meshes/mesh-instance.ts
+++ src/voidgl/base-layers/meshes/mesh-instance.ts
@@ -1,11 +1,11 @@
 import { Instance } from "../../instance-provider/instance";
-import { Mat4, Vec3, Vec4 } from "../../util";
+import { Mat4x4, Vec3, Vec4 } from "../../util";
 
 export class MeshInstance extends Instance {
   position: Vec3 = [0, 0, 0];
   scale: Vec3 = [1, 1, 1];
   quaternion: Vec4 = [0, 0, 0, 1];
-  transform: Mat4 =
+  transform: Mat4x4 =
     [1, 0, 0, 0,
      0, 1, 0, 0,
      0, 0, 1, 0,

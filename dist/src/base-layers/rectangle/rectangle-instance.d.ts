8c18ddbeee5396217a159d5d5c3dc137a5cd55b0
Autorelease Script <samuel+voidbot@voidray.co>
2019-05-09 16:07:36 +0000

    Release 1.28.1

--- dist/src/base-layers/rectangle/rectangle-instance.d.ts
+++ dist/src/base-layers/rectangle/rectangle-instance.d.ts
@@ -1,25 +1,22 @@
 import { IInstanceOptions, Instance } from "../../instance-provider/instance";
+import { Vec2 } from "../../util/vector";
 import { Anchor, ScaleMode } from "../types";
 export interface IRectangleInstanceOptions extends IInstanceOptions {
     anchor?: Anchor;
     depth?: number;
-    height?: number;
     scaling?: ScaleMode;
-    color: [number, number, number, number];
-    width?: number;
-    x?: number;
-    y?: number;
+    color?: [number, number, number, number];
+    position?: Vec2;
+    size?: Vec2;
 }
 export declare class RectangleInstance extends Instance {
     color: [number, number, number, number];
     depth: number;
-    height: number;
     maxScale: number;
     scale: number;
     scaling: ScaleMode;
-    width: number;
-    x: number;
-    y: number;
+    size: Vec2;
+    position: Vec2;
     private _anchor;
     constructor(options: IRectangleInstanceOptions);
     readonly anchor: Anchor;

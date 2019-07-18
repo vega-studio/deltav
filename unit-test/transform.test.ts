9197c2bf403aa9e43a7fb30aba4d258f14bb48af
heryoyo1989 <heryoyo1989@gmail.com>
2019-07-18 12:44:55 -0500

    change transform test

--- unit-test/transform.test.ts
+++ unit-test/transform.test.ts
@@ -10,11 +10,11 @@ const { exp, cos, sin, sqrt } = Math;
 
 const TO_RADIANS = Math.PI / 180;
 
-function fail4x4(actual: any, expected: any) {
+function fail4x4 (actual: any, expected: any) {
   return `\n\nACTUAL: ${toString4x4(actual)},\nEXPECTED: ${toString4x4(expected)}`;
 }
 
-function fuzzyCompare3(v1: Vec3, v2: Vec3): boolean {
+function fuzzyCompare3 (v1: Vec3, v2: Vec3): boolean {
   return (
     Math.abs(v1[0] - v2[0]) <= 1e-6 &&
     Math.abs(v1[1] - v2[1]) <= 1e-6 &&
@@ -22,7 +22,7 @@ function fuzzyCompare3(v1: Vec3, v2: Vec3): boolean {
   );
 }
 
-function assert1(actual: Vec1, expected: Vec1, shouldEqual: boolean = true) {
+function assert1 (actual: Vec1, expected: Vec1, shouldEqual: boolean = true) {
   if (shouldEqual) {
     assert.equal(compare1(actual, expected), true, fail1(actual, expected));
   }
@@ -32,7 +32,7 @@ function assert1(actual: Vec1, expected: Vec1, shouldEqual: boolean = true) {
   }
 }
 
-function assert3(actual: Vec3, expected: Vec3, shouldEqual: boolean = true) {
+function assert3 (actual: Vec3, expected: Vec3, shouldEqual: boolean = true) {
   if (shouldEqual) {
     assert.equal(compare3(actual, expected), true, fail3(actual, expected));
   }
@@ -42,7 +42,7 @@ function assert3(actual: Vec3, expected: Vec3, shouldEqual: boolean = true) {
   }
 }
 
-function assert4(actual: Quaternion, expected: Quaternion, shouldEqual: boolean = true) {
+function assert4 (actual: Quaternion, expected: Quaternion, shouldEqual: boolean = true) {
   if (shouldEqual) {
     assert.equal(compare4(actual, expected), true, fail4(actual, expected));
   }
@@ -52,7 +52,7 @@ function assert4(actual: Quaternion, expected: Quaternion, shouldEqual: boolean 
   }
 }
 
-function fuzzyAssertNumber(actual: number, expected: number, shouldEqual: boolean = true) {
+function fuzzyAssertNumber (actual: number, expected: number, shouldEqual: boolean = true) {
   if (shouldEqual) {
     assert.equal(actual - expected <= 1e07, true, fail1([actual], [expected]));
   }
@@ -62,7 +62,7 @@ function fuzzyAssertNumber(actual: number, expected: number, shouldEqual: boolea
   }
 }
 
-function fuzzyAssert3(actual: Vec3, expected: Vec3, shouldEqual: boolean = true) {
+function fuzzyAssert3 (actual: Vec3, expected: Vec3, shouldEqual: boolean = true) {
   if (shouldEqual) {
     assert.equal(fuzzyCompare3(actual, expected), true, fail3(actual, expected));
   }
@@ -72,7 +72,7 @@ function fuzzyAssert3(actual: Vec3, expected: Vec3, shouldEqual: boolean = true)
   }
 }
 
-function fuzzyAssert4(actual: Quaternion, expected: Quaternion, shouldEqual: boolean = true) {
+function fuzzyAssert4 (actual: Quaternion, expected: Quaternion, shouldEqual: boolean = true) {
   if (shouldEqual) {
     assert.equal(fuzzCompare4(actual, expected), true, fail4(actual, expected));
   }
@@ -82,7 +82,7 @@ function fuzzyAssert4(actual: Quaternion, expected: Quaternion, shouldEqual: boo
   }
 }
 
-function assert4x4(actual: Mat4x4, expected: Mat4x4, shouldEqual: boolean = true) {
+function assert4x4 (actual: Mat4x4, expected: Mat4x4, shouldEqual: boolean = true) {
   if (shouldEqual) {
     assert.equal(compare4x4(actual, expected), true, fail4x4(actual, expected));
   }
@@ -94,30 +94,30 @@ function assert4x4(actual: Mat4x4, expected: Mat4x4, shouldEqual: boolean = true
 
 describe('Transforms', () => {
   describe('Matrix', () => {
-    it ('Should be identity', () => {
+    it('Should be identity', () => {
       const t = new Transform();
       assert4x4(t.matrix, identity4());
     });
 
-    it ('Should be identity after lookAt', () => {
+    it('Should be identity after lookAt', () => {
       const t = new Transform();
       t.lookAt([0, 0, -1], [0, 1, 0]);
       assert4x4(t.matrix, identity4());
     });
 
-    it ('Should be identity for the view', () => {
+    it('Should be identity for the view', () => {
       const t = new Transform();
       assert4x4(t.viewMatrix, identity4());
     });
   });
 
   describe('Vector', () => {
-    it ('Should rotate on the y-axis', () => {
+    it('Should rotate on the y-axis', () => {
       const t = new Transform();
       t.lookAt([-1, 0, -1], [0, 1, 0]);
       const v: Vec4 = [0, 0, -1, 1];
 
-      fuzzyAssert4(transform4(t.matrix, v), [-sqrt(0.5), 0, -sqrt(0.5), 1]);
+      fuzzyAssert4(transform4(t.matrix, v), [sqrt(0.5), 0, -sqrt(0.5), 1]);
     });
   });
 });

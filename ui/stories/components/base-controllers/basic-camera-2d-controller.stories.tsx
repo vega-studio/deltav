327353f6f93a55820d5fe6bb151869ed7bd3d545
Diniden <diniden@outlook.com>
2025-07-01 20:07:56 -0600

    Added story for latest drag pan disable feature

--- ui/stories/components/base-controllers/basic-camera-2d-controller.stories.tsx
+++ ui/stories/components/base-controllers/basic-camera-2d-controller.stories.tsx
@@ -182,3 +182,87 @@ export const Basic_Camera_2D_Controller_With_Wheel_Scroll: StoryFn = (() => {
     </SurfaceJSX>
   );
 }).bind({});
+
+export const Basic_Camera_2D_Controller_With_Drag_Panning_Disabled: StoryFn =
+  (() => {
+    const circleProvider = React.useRef<InstanceProvider<CircleInstance>>(null);
+    const circles = React.useRef<CircleInstance[]>([]);
+    const camera = React.useRef(new Camera2D());
+    const ready = React.useRef(new PromiseResolver<Surface>());
+
+    useLifecycle({
+      async didMount() {
+        // Wait for the surface to establish the full pipeline
+        const surface = await ready.current.promise;
+        if (!circleProvider.current) return;
+
+        const size = surface.getViewSize("main");
+        if (!size) {
+          console.warn("Invalid View Size", surface);
+          return;
+        }
+
+        const instances: CircleInstance[] = [];
+
+        for (let i = 0, iMax = 100; i < iMax; ++i) {
+          instances.push(
+            circleProvider.current.add(
+              new CircleInstance({
+                center: [
+                  Math.random() * 400 - 200 + size.mid[0],
+                  Math.random() * 400 - 200 + size.mid[1],
+                ],
+                radius: Math.random() * 5 + 2,
+                color: [
+                  0,
+                  Math.random() * 0.8 + 0.2,
+                  Math.random() * 0.8 + 0.2,
+                  1,
+                ],
+              })
+            )
+          );
+
+          circles.current = instances;
+        }
+
+        return () => {};
+      },
+    });
+
+    return (
+      <SurfaceJSX
+        ready={ready.current}
+        options={{
+          alpha: true,
+          antialias: true,
+        }}
+      >
+        <BasicCamera2DControllerJSX
+          config={{
+            camera: camera.current,
+            wheelShouldScroll: true,
+          }}
+          disableDragPanning={true}
+        />
+        <ViewJSX
+          name="main"
+          type={View2D}
+          config={{
+            camera: camera.current,
+            background: [0, 0, 0, 1],
+            clearFlags: [ClearFlags.COLOR, ClearFlags.DEPTH],
+          }}
+        />
+        <LayerJSX
+          type={CircleLayer}
+          providerRef={circleProvider}
+          config={{
+            animate: {
+              center: AutoEasingMethod.easeInOutCubic(2000),
+            },
+          }}
+        />
+      </SurfaceJSX>
+    );
+  }).bind({});
